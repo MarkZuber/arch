d69e247c4663c5c0ab9c505b5eb60a77f09df25d
Mark Zuber <mzuber@zubexpspop.local>
2020-03-17 22:28:40 -0700

    create scripts

---
+++ chroot.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Borrowed and modified from https://github.com/LukeSmithxyz/LARBS/blob/master/testing/chroot.sh
+
+passwd
+
+TZuser=$(cat tzfinal.tmp)
+
+ln -sf /usr/share/zoneinfo/$TZuser /etc/localtime
+
+hwclock --systohc
+
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "en_US ISO-8859-1" >> /etc/locale.gen
+locale-gen
+
+pacman --noconfirm --needed -S networkmanager
+systemctl enable NetworkManager
+systemctl start NetworkManager
+
+pacman --noconfirm --needed -S grub && grub-install --target=i386-pc /dev/sda && grub-mkconfig -o /boot/grub/grub.cfg
+pacman --noconfirm --needed -S dialog
---
+++ installarch.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Borrowed and modified from https://github.com/LukeSmithxyz/LARBS/blob/master/testing/arch.sh
+
+#DO NOT RUN THIS YOURSELF because Step 1 is it reformatting /dev/sda WITHOUT confirmation,
+#which means RIP in peace qq your data unless you've already backed up all of your drive.
+
+pacman -Sy --noconfirm dialog || { echo "Error at script start: Are you sure you're running this as the root user? Are you sure you have an internet connection?"; exit; }
+
+dialog --defaultno --title "WARNING!" --yesno "This is an Arch install script .\n\nOnly run this script if you don't mind deleting your entire /dev/sda drive."  15 60 || exit
+dialog --defaultno --title "NO SERIOUSLY!" --yesno "This will delete ALL of /dev/sda and reinstall Arch.\n\nTo stop this script, press no."  10 60 || exit
+dialog --no-cancel --inputbox "Enter a name for your computer." 10 60 2> comp
+dialog --defaultno --title "Time Zone select" --yesno "Do you want use the default time zone(America/Los_Angeles)?.\n\nPress no for select your own time zone"  10 60 && echo "America/Los_Angeles" > tz.tmp || tzselect > tz.tmp
+
+dialog --no-cancel --inputbox "Enter swap in GB, then a space, then root partition size in GB." 10 60 2>psize
+IFS=' ' read -ra SIZE <<< $(cat psize)
+
+re='^[0-9]+$'
+if ! [ ${#SIZE[@]} -eq 2 ] || ! [[ ${SIZE[0]} =~ $re ]] || ! [[ ${SIZE[1]} =~ $re ]] ; then
+    SIZE=(12 25);
+fi
+
+timedatectl set-ntp true
+
+cat <<EOF | fdisk /dev/sda
+o
+n
+p
+
+
++200M
+n
+p
+
+
++${SIZE[0]}G
+n
+p
+
+
++${SIZE[1]}G
+n
+p
+
+
+w
+EOF
+partprobe
+
+yes | mkfs.ext4 /dev/sda4
+yes | mkfs.ext4 /dev/sda3
+yes | mkfs.ext4 /dev/sda1
+mkswap /dev/sda2
+swapon /dev/sda2
+mount /dev/sda3 /mnt
+mkdir -p /mnt/boot
+mount /dev/sda1 /mnt/boot
+mkdir -p /mnt/home
+mount /dev/sda4 /mnt/home
+
+pacman -Sy --noconfirm archlinux-keyring
+
+pacstrap /mnt base base-devel linux linux-firmware neovim
+
+genfstab -U /mnt >> /mnt/etc/fstab
+cat tz.tmp > /mnt/tzfinal.tmp
+rm tz.tmp
+mv comp /mnt/etc/hostname
+curl https://raw.githubusercontent.com/MarkZuber/arch/master/chroot.sh > /mnt/chroot.sh && arch-chroot /mnt bash chroot.sh && rm /mnt/chroot.sh
+
+dialog --defaultno --title "Final Qs" --yesno "Reboot computer?"  5 30 && reboot
+dialog --defaultno --title "Final Qs" --yesno "Return to chroot environment?"  6 30 && arch-chroot /mnt
+clear
